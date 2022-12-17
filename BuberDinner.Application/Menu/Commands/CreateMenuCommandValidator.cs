ce263e1229a48c94cf8240bdf4ccd7aa89ff72f5
Boris Korobeinikov <slon2k@gmail.com>
2022-12-17 18:52:43 +0600

    created Create Menu Validator

---
+++ BuberDinner.Application/Menu/Commands/CreateMenuCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace BuberDinner.Application.Menu.Commands
+{
+    public class CreateMenuCommandValidator : AbstractValidator<CreateMenuCommand>
+    {
+        public CreateMenuCommandValidator() 
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.HostId).NotEmpty();
+            RuleFor(x => x.Sections).NotEmpty();
+        }
+    }
+}
